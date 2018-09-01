f97c7a2570ef7e66c53b7e523646006c3c9fca37
thiruandroid <satkunamt@yahoo.com>
2018-09-01 15:23:38 +0100

    Adding rx mapping to get lips coordinates

--- core/src/main/java/com/beauty/face/core/MakeupPresenter.kt
+++ core/src/main/java/com/beauty/face/core/MakeupPresenter.kt
@@ -1,4 +1,12 @@
 package com.beauty.face.core
 
+import io.reactivex.Single
+
 class MakeupPresenter {
+
+    fun attach() {}
+
+    fun getFaceLandmarkForiLps() : Single<String> = Single.just("")
+
+    fun detach() {}
 }
