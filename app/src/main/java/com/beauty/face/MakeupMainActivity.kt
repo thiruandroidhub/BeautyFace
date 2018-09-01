6e2a1a4bc1f14de98ab902415817ab14c5633a8c
thiruandroid <satkunamt@yahoo.com>
2018-09-01 15:26:56 +0100

    Deleting unnecessary files

--- app/src/main/java/com/beauty/face/MakeupMainActivity.kt
+++ app/src/main/java/com/beauty/face/MakeupMainActivity.kt
@@ -1,5 +1,11 @@
 package com.beauty.face
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MakeupMainActivity : AppCompatActivity()
+class MakeupMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
