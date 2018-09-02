5b443d31508006b4d12e87e3be8f76e711d86023
thiruandroid <satkunamt@yahoo.com>
2018-09-02 01:49:55 +0100

    Adding dlib module

--- app/src/main/java/com/beauty/face/MakeupMainActivity.kt
+++ app/src/main/java/com/beauty/face/MakeupMainActivity.kt
@@ -2,8 +2,12 @@ package com.beauty.face
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.tzutalin.dlib.FaceDet
 
 class MakeupMainActivity : AppCompatActivity() {
+
+    internal var mFaceDet: FaceDet? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
